6b997e8191d7c97b2e52ac887b6c88ec04e9942b
esd <mobile@esd.mx>
2018-10-06 11:50:37 -0500

    v0.1.1

--- js/transfer.js
+++ js/transfer.js
@@ -14,10 +14,13 @@ function exporta() {
             text = text + element.doc.timestamp + ",";
             text = text + element.doc.amount + ",";
             text = text + element.doc.description + ",";
-            text = text + element.doc._id.split("!")[1] + "\n";
+            text = text + element.doc._id.split("!")[1];
+            if (i < result.rows.length) {
+              text = text + "\n";
+            }
         });
         document.getElementById("areatxt").value = text;
-        document.getElementById("areatxt").style.height= (16*i)+20 + "px";
+        document.getElementById("areatxt").style.height= (18*i)+20 + "px";
     });
   }
 
@@ -69,21 +72,25 @@ function exporta() {
     newdata= document.getElementById("areatxt").value.split("\n");
 
     doc = cuentas;
+    var i = 0;
 
     newdata.forEach(row => {
         var columna = row.split(",")
-        doc.cuantas = doc.cuantas + 1;
 
         var category = emojitocat(columna[1]);
         var today = columna[2];
         var lana = (Number(columna[3]) || 0);
         var memo = columna[4];
-
-        doc.total = doc.total + (Number(lana) || 0);
-        doc[category] = doc[category] + lana;
+        
         
         mov = {};
-        mov._id = "mov_" + today + " !"+ s4()+s4();
+        var key = s4()+s4();
+        if (typeof(columna[5])!="undefined") {
+          if (columna[5]!="") {
+            key = columna[5];
+          }
+        }
+        mov._id = "mov_" + today + " !"+ key;
         // aquí se puede implementar el no duplicar usando el id del movimiento importado
         // idea : cuando hay id : leer bd por mismo id y actualizar en vez de agregar
         mov.contador = "mov_" + doc.cuantas;
@@ -92,12 +99,34 @@ function exporta() {
         mov.amount = lana;
         mov.category = category;
         console.log(mov);
-        db.put(mov);
+        db.put(mov, function(err,respuesta) {
+          i++
+          if (err) {
+            return console.log(err);
+          } else {
+            doc.total = doc.total + (Number(lana) || 0);
+            doc[category] = doc[category] + lana;
+            doc.cuantas = doc.cuantas + 1;
+          }
+          console.log(i);
+          if (i == newdata.length) {
+            db.put(doc, function(err,response) {
+              db.get('accounts',function(err, doc) {
+                if(err) {
+                  console.log("Error al grabar movimiento");
+                 } else {
+                      cuentas = doc;
+                      populateAcc();
+                 }
+                });
+            });
+          }
+        });
         
     })
     ga('send', 'event', 'Expense', 'Import');
   
-    db.put(doc);
-    populateAcc();
+    
+    
 
   }

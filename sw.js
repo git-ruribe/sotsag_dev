660b0014ffbacc8903b4614b7191d3dbb482975b
esd <mobile@esd.mx>
2018-10-07 09:07:54 -0500

    v0.1.2

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-console.log('Hello from sw.js');
+console.log('Hello from sw.js v0.1');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
